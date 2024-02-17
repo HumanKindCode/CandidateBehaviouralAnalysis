a7c732dd0baf0acefce7d5449372c8bca088b48a
HumanKindCode <156196457+HumanKindCode@users.noreply.github.com>
2024-02-17 12:24:12 +1100

    Update redisDB.py

--- redisDB.py
+++ redisDB.py
@@ -2,46 +2,28 @@ import streamlit as st
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-username = 'humankindau'
-password = '1B0mB9GKxyBHYBnk'
-cluster_hostname = 'cluster0.n9wsfyk.mongodb.net'
-database_name = 'CandidateAnswers'
-
-# MongoDB connection
-uri = f'mongodb+srv://{username}:{password}@cluster0.pye7uj8.mongodb.net/?retryWrites=true&w=majority'
+# MongoDB setup
+uri = "mongodb+srv://humankindau:oyNSzBe9qyphEt6b@cluster0.8gp5zlf.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri, server_api=ServerApi('1'))
-db = client['streamlit_app']  # Use your database name
-collection = db['user_data']  # Use your collection name
+db = client['CandidateResponses']  # Specify your database name
+collection = db['CandidateAnswers']  # Specify your collection name
 
 # Streamlit app
 def main():
-    st.title('MongoDB Data Storage App')
+    st.title('MongoDB Data Insertion App')
 
-    # User input
-    user_name = st.text_input('Enter your name:')
-    user_data = st.text_area('Enter some data to store in MongoDB:')
+    # User input fields
+    data_title = st.text_input('Data Title:', '')
+    data_content = st.text_area('Data Content:', '')
 
-    # When the button is pressed, store the data in MongoDB
-    if st.button('Store Data'):
-        if user_name and user_data:
-            # Insert data into MongoDB
-            document = {'name': user_name, 'data': user_data}
+    # Insert data into MongoDB
+    if st.button('Insert Data'):
+        if data_title and data_content:
+            document = {'title': data_title, 'content': data_content}
             collection.insert_one(document)
-            st.success('Data stored successfully in MongoDB!')
+            st.success('Data inserted successfully!')
         else:
-            st.error('Please fill in both fields.')
-
-    # Retrieving data
-    st.subheader('Retrieve Data')
-    search_name = st.text_input('Enter a name to retrieve data from MongoDB:', key='search')
-    
-    if st.button('Retrieve Data', key='retrieve'):
-        # Use the name to retrieve the data from MongoDB
-        document = collection.find_one({'name': search_name})
-        if document:
-            st.write(f'Data for {search_name}: {document["data"]}')
-        else:
-            st.error(f'No data found for {search_name} in MongoDB.')
+            st.error('Please fill in all fields.')
 
 if __name__ == '__main__':
     main()
